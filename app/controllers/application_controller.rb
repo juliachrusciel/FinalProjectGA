f231e9867fb2ef49d5aad669ae3d6e577022639c
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-06-05 15:43:57 -0500

    Adds CSS changes to application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def current_user
-  #   if session[]
-  # end
+  def authenticate!
+    if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+
+  def admin_authenticate!
+    redirect_to '/' unless (current_user && current_user.is_admin)
+  end
 
 end
